1e998f8bd292581699c338305b8741e0cd8c8d13
Eddie <mchh.004@gmail.com>
2020-01-08 17:33:14 -0500

    Update person.rb

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
-# your code goes here
+class Person
+    attr_accessor :bank_account, :happiness, :hygiene
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name, bank_account=25, happiness=8, hygiene=8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness.clamp(0,10)
+        @hygiene = hygiene.clamp(0,10)
+
+        @@all << self
+    end
+
+
+end
